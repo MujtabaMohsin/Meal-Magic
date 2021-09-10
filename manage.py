6aed21946b475d990e87cbb07a7800c9030c7e1f
Muj <mmjj1010@gmail.com>
2021-09-10 08:05:03 +0300

    initial commit3

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app
-from models import db
+from app import create_app
+from models import *
 
-migrate = Migrate(app, db)
-manager = Manager(app)
+migrate = Migrate(create_app, db)
+manager = Manager(create_app)
 
 manager.add_command('db', MigrateCommand)
 
